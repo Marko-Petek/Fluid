cf83808237c0753b82d40b9abb98e9d1a9028bbf
Marko-Petek <markopetek@protonmail.com>
2019-02-18 19:51:21 +0100

    After theory presentation.

---
+++ Anm/L03_Percolation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L03_Percolation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Anm/readme.md
@@ -0,0 +1,1 @@
+Contains homeworks for Advanced Numerical Methods.
