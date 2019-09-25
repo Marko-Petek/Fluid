60d0f2d561ff3ddcf50de860e02c6968d62e9e0f
Marko Petek <markopetek@protonmail.com>
2019-09-25 16:57:34 +0200

    Reporter not working.

--- Demos/CavityFlow/Program.cs
+++ Demos/CavityFlow/Program.cs
@@ -6,8 +6,11 @@ using static Fluid.Internals.Development.Reporter;
 namespace Fluid.Demos.CavityFlow {
    /// <summary>Driven cavity flow problem is a very simple problem with which methods are initially tested.</summary>
    class Program {
-      static void Main() => EntryPointSetup("Starting CavityFlow.",
-         () => Run1(), VerbositySettings.Moderate);
+      static void Main() {
+         EntryPointSetup("Starting CavityFlow.",
+            () => Run1(), VerbositySettings.Moderate);
+         System.Console.WriteLine("Finished.");
+      } 
 
       static void Run1() {
          var cavFlow = new CavityFlow(0.1, 0.1, 3) ;
