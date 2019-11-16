0e03f9bc3791bcbbf4270011093f31c23b60b51e
Marko-Petek <markopetek@protonmail.com>
2019-11-16 17:22:03 +0100

    bruh

--- Internals/Lsfem/ISimInit.cs
+++ Internals/Lsfem/ISimInit.cs
@@ -1,5 +1,17 @@
+using System;
+using System.Collections.Generic;
+using dbl = System.Double;
+using DA = Fluid.Internals.Numerics.DblArithmetic;
+using My = Fluid.Internals.Collections.Custom;
 namespace Fluid.Internals.Lsfem {
+   using Vec = Fluid.Internals.Collections.Vector<dbl,DA>;
+   using Tnr = Fluid.Internals.Collections.Tensor<dbl, DA>;
+   using Voids = Voids<dbl,DA>;
+   using Lst = List<int>;
+   using VecLst = My.List<Vec2>;
+   using PE = PseudoElement;
+
 interface ISimInit {
-   
+   (int newCurrGInx, Dictionary<string,PE[][]>) CreatePatches(int currGinx);
 }
 }
