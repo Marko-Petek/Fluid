0df9e41998e7b854f866dab24106d4c5d066d4c1
Marko Petek <markopetek@protonmail.com>
2020-01-18 13:00:21 +0100

    vik

--- Internals/Numerics/F2D.cs
+++ Internals/Numerics/F2D.cs
@@ -4,7 +4,7 @@ using dbl = System.Double;
 using f2da = Fluid.Internals.Numerics.Func2DArithmetic;
 
 namespace Fluid.Internals.Numerics {
-   /// <summary>A custom function of 2 arguments on the domain [-1,1]x[-1,1].</summary>
+   /// <summary>A custom function of 2 arguments on the domain [-1,1]x[-1,1]. Note: always use One as 1 and Zero as 0 because the equality checks for those inside operations rely on reference comparisons.</summary>
    public class F2D : IEquatable<F2D>, IComparable<F2D> {
       public static F2D One { get; } = new F2D((x,y) => 1.0);
       public static F2D Zero { get; } = new F2D((x,y) => 0.0);
@@ -43,38 +43,38 @@ namespace Fluid.Internals.Numerics {
          get => F(x,y);
       }
       public static F2D operator *(F2D f1, F2D f2) {                          // FIXME: Streamline Func2D
-         if(f1.Equals(One))
+         if(f1 == One)
             return f2;
-         else if(f2.Equals(One)) 
+         else if(f2 == One) 
             return f1;
-         else if(f1.Equals(Zero) || f2.Equals(Zero))
+         else if(f1 == Zero || f2 == Zero)
             return Zero;
          else
             return new F2D( (x,y) => f1[x,y] * f2[x,y] );
       }
       public static F2D TripProd(F2D f1, F2D f2, F2D f3) {
-         if(f1.Equals(One))
+         if(f1 == One)
             return f2 * f3;
-         else if(f2.Equals(One))
+         else if(f2 == One)
             return f1 * f3;
-         else if(f3.Equals(One))
+         else if(f3 == One)
             return f1 * f2;
-         else if(f1.Equals(Zero) || f2.Equals(Zero) || f2.Equals(Zero))
+         else if(f1 == Zero || f2 == Zero || f2 == Zero)
             return Zero;
          else
             return new F2D( (x,y) => f1[x,y]*f2[x,y]*f3[x,y] );
       }
 
       public static F2D QuadProd(F2D f1, F2D f2, F2D f3, F2D f4) {
-         if(f1.Equals(One))
+         if(f1 == One)
             return TripProd(f2, f3, f4);
-         else if(f2.Equals(One))
+         else if(f2 == One)
            return TripProd(f1, f3, f4);
-         else if(f3.Equals(One))
+         else if(f3 == One)
             return TripProd(f1, f2, f4);
-         else if(f4.Equals(One))
+         else if(f4 == One)
             return TripProd(f1, f2, f3);
-         else if(f1.Equals(Zero) || f2.Equals(Zero) || f3.Equals(Zero) || f4.Equals(Zero))
+         else if(f1 == Zero || f2 == Zero || f3 == Zero || f4 == Zero)
             return Zero;
          else
             return new F2D( (x,y) => f1[x,y]*f2[x,y]*f3[x,y]*f4[x,y] );
@@ -82,17 +82,17 @@ namespace Fluid.Internals.Numerics {
          
 
       public static F2D QuintProd(F2D f1, F2D f2, F2D f3, F2D f4, F2D f5) {
-         if(f1.Equals(One))
+         if(f1 == One)
             return QuadProd(f2, f3, f4, f5);
-         else if(f2.Equals(One))
+         else if(f2 == One)
            return QuadProd(f1, f3, f4, f5);
-         else if(f3.Equals(One))
+         else if(f3 == One)
             return QuadProd(f1, f2, f4, f5);
-         else if(f4.Equals(One))
+         else if(f4 == One)
             return QuadProd(f1, f2, f3, f5);
-         else if(f5.Equals(One))
+         else if(f5 == One)
             return QuadProd(f1, f2, f3, f4);
-         else if(f1.Equals(Zero) || f2.Equals(Zero) || f3.Equals(Zero) || f4.Equals(Zero) || f5.Equals(Zero))
+         else if(f1 == Zero || f2 == Zero || f3 == Zero || f4 == Zero || f5 == Zero)
             return Zero;
          else
             return new F2D( (x,y) => f1[x,y]*f2[x,y]*f3[x,y]*f4[x,y]*f5[x,y] );
