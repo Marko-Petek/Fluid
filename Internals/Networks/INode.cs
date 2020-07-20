0785a4a7e57bd68c892541802d6a6aa5cc704105
Marko-Petek <ketepokram@gmail.com>
2020-07-21 01:15:15 +0200

    vik

---
+++ Internals/Networks/INode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fluid.Internals.Networks {
+/// <summary>A node is a collection of references to other nodes.</summary>
+   public interface INode {
+      
+      protected Dictionary<INode,int[]> Bonds { get; }       // (which node it is connected to; sequential connection slot number)
+
+      // C parts of Bonds have negative values.
+      // P parts have positive values.
+   }
+}
