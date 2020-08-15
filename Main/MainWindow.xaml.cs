8cc4e9f94e57bc0311f17d1908a2eb469cfb909f
Marko-Petek <ketepokram@gmail.com>
2020-08-15 13:24:14 +0200

    vik

--- Main/MainWindow.xaml.cs
+++ Main/MainWindow.xaml.cs
@@ -6,15 +6,12 @@ using Avalonia.Controls.Html;
 namespace Fluid {
 public class MainWindow : Window {
 
-   private HtmlControl HtmlCon { get; set; }
    public MainWindow() {
       InitializeComponent();
    }
 
    private void InitializeComponent() {
       AvaloniaXamlLoader.Load(this);
-      HtmlCon = this.FindControl<HtmlControl>("HtmlCon");
-      HtmlCon.Text = "<p>A Markdown string with <strong>emphasis</strong>.</p>";
    }
    
 }
